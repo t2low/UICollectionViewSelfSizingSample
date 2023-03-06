71c6107f61b84f00ab4c5fc835acd6fcef91d0f8
Tetsuro Nakamura <t2low.n@gmail.com>
2023-03-07 00:12:16 +0900

    Inital commit

---
+++ UICollectionViewSample/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  UICollectionViewSample
+//
+//  Created by Tetsuro Nakamura on 2023/03/07.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
